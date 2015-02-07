7ff4f48d15a70c772c1ee8e6647a918169839650
Stefan Suciu <stefan@s2i2.ro>
2015-02-07 12:14:36 +0200

    Add POD.

--- lib/App/Transfer/Recipe/Transform/Types.pm
+++ lib/App/Transfer/Recipe/Transform/Types.pm
@@ -14,3 +14,36 @@ coerce 'ArrayRefFromStr', from 'Str', via { [ $_ ] };
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 Name
+
+App::Transfer::Recipe::Transform::Types - Definition of attribute data types
+
+=head1 Synopsis
+
+  use App::Transfer::Recipe::Transform::Types;
+
+=head1 Description
+
+This module defines data types use in Transfer object
+attributes. Supported types are:
+
+=over
+
+=item C<ArrayRefFromStr>
+
+An array reference coerced from string.
+
+=back
+
+=head1 Author
+
+Ștefan Suciu <stefan@s2i2.ro>
+
+=head1 License
+
+=cut
