4712dcea050ad68bf35460d25dfcf9120c9671b0
Stefan Suciu <stefan@s2i2.ro>
2015-02-07 12:07:39 +0200

    Remove reader method, add POD.

--- lib/App/Transfer/Reader.pm
+++ lib/App/Transfer/Reader.pm
@@ -30,15 +30,6 @@ has 'options' => (
     required => 1,
 );
 
-sub reader {
-    my $class = ref $_[0] || shift;
-    return '' if $class eq __PACKAGE__;
-    my $pkg = quotemeta __PACKAGE__;
-    $class =~ s/^$pkg\:://;
-    $class =~ s/_/-/g;
-    return $class;
-}
-
 sub load {
     my ( $class, $p ) = @_;
 
@@ -74,3 +65,80 @@ sub load {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 Name
+
+App::Transfer::Reader - Base class for the reader interface
+
+=head1 Synopsis
+
+  ok my $reader = App::Transfer::Reader->load({
+      transfer => $transfer,
+      recipe   => $recipe,
+      reader   => 'excel',
+      options  => $options,
+  });
+  my $records = $reader->get_data;
+
+=head1 Description
+
+App::Transfer::Reader is the base class for all reader modules.
+
+=head1 Interface
+
+=head2 Constructors
+
+=head3 C<load>
+
+  my $reader = App::Transfer::Reader->load( \%params );
+
+A factory method for instantiating Transfer readers. It loads the
+subclass for the specified reader and calls C<new> with the hash
+parameter. Supported parameters are:
+
+=over
+
+=item C<transfer>
+
+The App::Transfer object.
+
+=item C<recipe>
+
+An L<App::Transfer::Recipe> representing the recipe in use.
+
+=item C<reader>
+
+The name of the reader to be used.
+
+=item C<options>
+
+An L<App::Transfer::Options> representing the options and configs
+passed and read by the application.
+
+=back
+
+=head2 Attributes
+
+=head3 C<transfer>
+
+  my $transfer = $self->transfer;
+
+Returns the L<App::Transfer> object that instantiated the reader.
+
+=head3 C<recipe>
+
+  my $recipe = $self->recipe;
+
+Returns the L<App::Transfer::Recipe> object that instantiated the reader.
+
+=head3 C<options>
+
+  my $options = $self->options;
+
+Returns the L<App::Transfer::Options> object that instantiated the reader.
+
+=cut
