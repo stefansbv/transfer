518b0b8bae745ccbde7df9a45856cbfbdc4313bf
Stefan Suciu <stefan@s2i2.ro>
2015-02-07 12:00:49 +0200

    Add PODs.

--- lib/App/Transfer/Plugin/join_fields.pm
+++ lib/App/Transfer/Plugin/join_fields.pm
@@ -1,6 +1,6 @@
 package App::Transfer::Plugin::join_fields;
 
-# ABSTRACT: join_fields
+# ABSTRACT: # ABSTRACT: Transfer plugin for join_fields
 
 use 5.010001;
 use Moose;
@@ -9,12 +9,48 @@ use namespace::autoclean;
 with 'MooseX::Log::Log4perl';
 
 sub join_fields {
-    my ($self, $p) = @_;
-    my ($logfld, $logidx, $field, $value, $separator ) = @$p{qw(logfld logidx name value separator)};
-    return unless ref $value;
-    return join $separator, @{$value};
+    my ( $self, $p ) = @_;
+    my ( $logfld, $logidx, $field, $text, $separator )
+        = @$p{qw(logfld logidx name value separator)};
+    return unless ref $text;
+    return join $separator, @{$text};
 }
 
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 Name
+
+App::Transfer::Plugin::join_field - Transfer plugin for joining text
+
+=head1 Interface
+
+=head2 Instance Methods
+
+=head3 C<join_fields>
+
+Parameters:
+
+=over
+
+=item C<$logfld>    log field name
+
+=item C<$logidx>    log field value
+
+=item C<$field>     field name
+
+=item C<$text>      field value
+
+=item C<$separator> the separator char
+
+=back
+
+The C<join_fields> method uses the C<join> function on the input
+C<text> and returns a string.
+
+=cut
--- lib/App/Transfer/Plugin/split_field.pm
+++ lib/App/Transfer/Plugin/split_field.pm
@@ -1,6 +1,6 @@
 package App::Transfer::Plugin::split_field;
 
-# ABSTRACT: split_field
+# ABSTRACT: # ABSTRACT: Transfer plugin for split_field
 
 use 5.010001;
 use Moose;
@@ -19,3 +19,40 @@ sub split_field {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 Name
+
+App::Transfer::Plugin::split_field - Transfer plugin for spliting text
+
+=head1 Interface
+
+=head2 Instance Methods
+
+=head3 C<split_field>
+
+Parameters:
+
+=over
+
+=item C<$logfld>    log field name
+
+=item C<$logidx>    log field value
+
+=item C<$field>     field name
+
+=item C<$text>      field value
+
+=item C<$limit>     the number of destination fields
+
+=item C<$separator> the separator char
+
+=back
+
+The C<split_field> method uses the C<split> function on the input
+C<text> and returns an array.
+
+=cut
