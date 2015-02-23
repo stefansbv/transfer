7c1ae9d199db8b38565da719fe5fd6520f1fd1fe
Stefan Suciu <stefan@s2i2.ro>
2015-02-23 12:28:58 +0200

    Add plugin for replacing all space like chars with one space.

---
+++ lib/App/Transfer/Plugin/one_space.pm
@@ -0,0 +1,51 @@
+package App::Transfer::Plugin::one_space;
+
+# ABSTRACT: Transfer plugin: no spaces
+
+use 5.010001;
+use Moose;
+use namespace::autoclean;
+
+with 'MooX::Log::Any';
+
+sub one_space {
+    my ($self, $p) = @_;
+    my ($logstr, $text, $len ) = @$p{qw(logstr value length)};
+    return unless $text;
+    $text =~ s{\s+}{ }gmx;
+    return $text;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 Name
+
+App::Transfer::Plugin::one_space - Transfer plugin: 'one_space'
+
+=head1 Interface
+
+=head2 Instance Methods
+
+=head3 C<one_space>
+
+Parameters:
+
+=over
+
+=item C<$logstr> log string
+
+=item C<$field>  field name
+
+=item C<$text>   field value
+
+=back
+
+The C<one_space> method replaces all space chars (regex: \s+) with a
+single space and returns the resulting string.
+
+=cut
