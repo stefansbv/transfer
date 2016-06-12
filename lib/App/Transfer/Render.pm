ffbc77f39edd794db83605ebbd522eaf5703194f
Stefan Suciu <stefbv70@gmail.com>
2016-06-12 12:33:28 +0300

    Refactor the Render module.

--- lib/App/Transfer/Render.pm
+++ lib/App/Transfer/Render.pm
@@ -6,64 +6,158 @@ use 5.010001;
 use utf8;
 use Moose;
 use Template;
-use File::ShareDir qw(dist_dir);
 use Path::Tiny;
+use Try::Tiny;
+use File::ShareDir qw(dist_dir);
+use MooseX::Types::Path::Tiny qw(File Path);
 use App::Transfer::X qw(hurl);
 use Locale::TextDomain qw(App-Transfer);
 use namespace::autoclean;
 
+has 'type' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => sub {
+        return 'recipe';
+    },
+);
+
+has 'data' => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 1,
+);
+
+has 'output_file' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has 'output_path' => (
+    is      => 'ro',
+    isa     => Path,
+    coerce  => 1,
+    default => sub {
+        return path '.';
+    },
+);
+
+has 'templ_path' => (
+    is      => 'ro',
+    isa     => Path,
+    coerce  => 1,
+    default => sub {
+        my $self = shift;
+        return dist_dir('App-Transfer')
+            ? path( dist_dir('App-Transfer'), 'templates' )
+            : path( 'share',                  'templates' );
+    },
+);
+
 sub render {
-    my ($self, $args) = @_;
+    my $self = shift;
+    my $type        = $self->type;
+    my $output_file = $self->output_file;
 
-    my $type        = $args->{type};
-    my $output_file = $args->{output_file};
-    my $data        = $args->{data};
-    my $output_path = $args->{output_path};
-    my $templ_path  = $args->{templ_path}
-        // path( dist_dir('App-Transfer'), 'templates' );
-
-    my $template = $self->get_template_for($type);
-
-    $output_file = "${type}$output_file" if $output_file =~ m{^\.};
+    my $template = try { $self->get_template }
+    catch {
+        hurl {
+            ident => 'render',
+                exitval => 1,
+                message => __x( 'Template error: {error}', error => $_ ),
+            };
+    };
 
     my $tt = Template->new(
-        INCLUDE_PATH => $templ_path,
-        OUTPUT_PATH  => $output_path,
+        INCLUDE_PATH => $self->templ_path,
+        OUTPUT_PATH  => $self->output_path->stringify,
     );
 
-    $tt->process( $template, $data, $output_file, binmode => ':utf8' )
-        or die $tt->error(), "\n";
+    $tt->process( $template, $self->data, $output_file, binmode => ':utf8' )
+        || hurl {
+            ident => 'render',
+                exitval => 1,
+                message => __x( 'Template error: {error}', error => $tt->error() ),
+            };
 
-    return $output_file;
+    return path($self->output_path, $output_file);
 }
 
-sub get_template_for {
-    my ($self, $type) = @_;
-
-    hurl "The type argument is required" unless defined $type;
-
+sub get_template {
+    my $self = shift;
+    my $type = $self->type;
     my $template =
-         $type eq q{}               ? die("Empty type argument")
-       : $type eq 'recipe'          ? 'recipe.tt'
-       :                              die("Unknown type $type")
-       ;
-
+         $type eq q{}      ? $self->template_type_error
+       : $type eq 'recipe' ? 'recipe.tt'
+       :                     $self->template_type_error;
     return $template;
+}
+
+sub template_type_error {
+    my $self = shift;
+    my $type = $self->type;
+    hurl {
+        ident => 'render',
+            exitval => 1,
+            message => __x "Template error, unknown type: '{type}'", type => $type,
+        };
 }
 
 1;
 
 __END__
 
-=head2 render
+=encoding utf8
+
+=head1 SYNOPSIS
+
+    my $args = {
+        data        => { r => $data },
+        output_file => 'test-render-recipe.conf',
+    };
+    my $atr  = App::Transfer::Render->new($args);
+    my $file = $atr->render;
+
+=head1 DESCRIPTION
 
 Generate a file from templates.
 
-Adapted from Tpda3-Devel :)
+=head1 INTERFACE
 
-=head2 get_template_for
+=head2 ATTRIBUTES
 
-Return the template name for one of the two known types: I<config> or
-I<recipe>.
+=head3 type
+
+Optional type attribute.  The default value is I<recipe> and is the
+only one recognized. :)
+
+=head3 data
+
+The data argument passed to the module has to be a hash reference with
+the key <r>;
+
+    data => { r => $data },
+
+=head3 output_file
+
+=head3 output_path
+
+=head3 templ_path
+
+Returns the C<File::ShareDir->dist_dir> if the app is installed or the
+local L<share/templates> dir.
+
+=head2 INSTANCE METHODS
+
+=head3 render
+
+=head3 get_template
+
+Return the template name.
+
+=head3 template_type_error
+
+Throw an exception in exceptional cases ;)
 
 =cut
