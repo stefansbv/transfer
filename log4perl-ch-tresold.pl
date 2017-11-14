107d466d040c61e4f11ce3adb556e7720639a0c7
Stefan Suciu <stefbv70@gmail.com>
2017-11-14 20:56:03 +0200

    Add example for changing treshold

---
+++ log4perl-ch-tresold.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use Log::Log4perl;
+
+# Log Levels
+#   There are six predefined log levels: "FATAL", "ERROR", "WARN", "INFO",
+#   "DEBUG", and "TRACE" (in descending priority). Your configured logging
+#   level has to at least match the priority of the logging message.
+
+#   If your configured logging level is "WARN", then messages logged with
+#   "info()", "debug()", and "trace()" will be suppressed. "fatal()",
+#   "error()" and "warn()" will make their way through, because their
+#   priority is higher or equal than the configured setting.
+
+my $log4p_conf = q(
+    log4perl.rootLogger=DEBUG, SCREEN
+    log4perl.appender.SCREEN=Log::Log4perl::Appender::Screen
+    log4perl.appender.SCREEN.layout=SimpleLayout
+    log4perl.appender.SCREEN.Threshold=WARN
+);
+
+Log::Log4perl->init(\$log4p_conf);
+
+my $log = Log::Log4perl->get_logger("");
+
+say "is trace: ", $log->is_trace ? 'true' : 'false';
+say "is_debug: ", $log->is_debug ? 'true' : 'false';
+say "is_info : ", $log->is_info  ? 'true' : 'false';
+say "is_warn : ", $log->is_warn  ? 'true' : 'false';
+say "is_error: ", $log->is_error ? 'true' : 'false';
+say "is_fatal: ", $log->is_fatal ? 'true' : 'false';
+
+say '---';
+$log->trace("trace msg");
+$log->debug("debug msg");
+$log->info("info msg");
+$log->warn("warn msg");
+$log->error("error msg");
+$log->fatal("fatal msg");
+
+# Change the treshold
+Log::Log4perl->appender_by_name('SCREEN')->threshold('INFO');
+
+$Log::Log4perl::Logger::APPENDER_BY_NAME{SCREEN}->threshold('DEBUG');
+
+say '---';
+$log->trace("trace msg");
+$log->debug("debug msg");
+$log->info("info msg");
+$log->warn("warn msg");
+$log->error("error msg");
+$log->fatal("fatal msg");
