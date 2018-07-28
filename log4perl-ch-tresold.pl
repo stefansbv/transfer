20469d651c38e567c869e6e1fdcc7dc698e3fe14
Stefan Suciu <stefbv70@gmail.com>
2018-07-28 20:11:37 +0300

    Rearange levels by threshold

--- log4perl-ch-tresold.pl
+++ log4perl-ch-tresold.pl
@@ -25,32 +25,38 @@ my $log4p_conf = q(
 
 Log::Log4perl->init(\$log4p_conf);
 
+say "Level from config is WARN";
+
 my $log = Log::Log4perl->get_logger("");
 
-say "is trace: ", $log->is_trace ? 'true' : 'false';
-say "is_debug: ", $log->is_debug ? 'true' : 'false';
-say "is_info : ", $log->is_info  ? 'true' : 'false';
-say "is_warn : ", $log->is_warn  ? 'true' : 'false';
-say "is_error: ", $log->is_error ? 'true' : 'false';
 say "is_fatal: ", $log->is_fatal ? 'true' : 'false';
+say "is_error: ", $log->is_error ? 'true' : 'false';
+say "is_warn : ", $log->is_warn  ? 'true' : 'false';
+say "is_info : ", $log->is_info  ? 'true' : 'false';
+say "is_debug: ", $log->is_debug ? 'true' : 'false';
+say "is trace: ", $log->is_trace ? 'true' : 'false';
 
 say '---';
-$log->trace("trace msg");
-$log->debug("debug msg");
-$log->info("info msg");
-$log->warn("warn msg");
-$log->error("error msg");
 $log->fatal("fatal msg");
+$log->error("error msg");
+$log->warn("warn msg");
+$log->info("info msg");
+$log->debug("debug msg");
+$log->trace("trace msg");
+say '---';
 
 # Change the treshold
 Log::Log4perl->appender_by_name('SCREEN')->threshold('INFO');
 
 $Log::Log4perl::Logger::APPENDER_BY_NAME{SCREEN}->threshold('DEBUG');
 
+say "Level from setting is DEBUG";
+
 say '---';
-$log->trace("trace msg");
-$log->debug("debug msg");
-$log->info("info msg");
-$log->warn("warn msg");
-$log->error("error msg");
 $log->fatal("fatal msg");
+$log->error("error msg");
+$log->warn("warn msg");
+$log->info("info msg");
+$log->debug("debug msg");
+$log->trace("trace msg");
+say '---';
