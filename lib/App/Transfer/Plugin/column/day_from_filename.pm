fb5633f821867fddd5fe4a42cfe9094e79e76e37
Stefan Suciu <stefbv70@gmail.com>
2020-03-22 20:34:44 +0200

    Plugins for extracting strings from the input file name

---
+++ lib/App/Transfer/Plugin/column/day_from_filename.pm
@@ -0,0 +1,110 @@
+package App::Transfer::Plugin::column::day_from_filename;
+
+# ABSTRACT: Transfer plugin: value for field from file name (TMYYMMZZ)
+
+use 5.010001;
+use Moose;
+use namespace::autoclean;
+
+with 'MooX::Log::Any';
+
+sub day_from_filename {
+    my ( $self, $p ) = @_;
+    my ( $logstr, $text, $pat ) = @$p{qw(logstr value pattern)};
+    say "day_from_filename: $text";
+    return unless $text;
+    die "The 'day_from_filename plugin' requires a pattern attribute"
+        unless $pat;
+    if ( my ( $i, $l ) = $self->params_from_pattern($pat) ) {
+        say "$i, $l";
+        my $value = substr( $text, $i, $l );
+        say "return '$value'";
+        return $value;
+    }
+    return;
+}
+
+sub params_from_pattern {
+    my ( $self, $pat ) = @_;
+    say "pat = $pat";
+    my $p1 = index $pat, '[';
+    say "p1 = $p1";
+    if ( $p1 > 0 ) {
+        my $p2 = index $pat, ']';
+        say "p2 = $p2";
+        return ( $p1, $p2 - 1 - $p1 );
+    }
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::Transfer::Plugin::day_from_filename - plugin: 'day_from_filename'
+
+Example recipe configuration:
+
+Transformation step (column):
+
+    <transform column>
+      <step>
+        type                = default_value
+        pattern             = TMYYMM[dd]
+        field               = day
+        method              = day_from_filename
+      </step>
+    </transform>
+
+=head1 INTERFACE
+
+=head2 INSTANCE METHODS
+
+=head3 C<day_from_filename>
+
+Parameters:
+
+=over
+
+=item C<$logstr> log string
+
+=item C<$text>   value
+
+=item C<$pat>    pattern
+
+=back
+
+The C<day_from_filename> method returns the number extracted from the
+input C<value> using the C<pattern> parameter.
+
+The C<pattern> is a string which provides the position of the text to
+be returned.  A '[' and a ']' character mark the substring to be
+returned.
+
+For example if we have a file name like C<TM200331> (without the
+extension) and a pattern like C<TMYYMM[dd]>, we expect the number 31
+to be returned by the function.
+
+The method can be extended with validation code to check if the file
+name starts with C<TM> and if the return string is a number or even a
+valid day number.
+
+=head3 C<params_from_pattern>
+
+The C<params_from_pattern method> calculates and returns the index and
+the lenght parameters for the substr function.
+
+Example:
+
+    pattern = TMYYMM[dd]
+      -> p1 = 6
+      -> p2 = 9
+        => (6, 2)
+
+=cut
--- lib/App/Transfer/Plugin/column/year_from_filename.pm
+++ lib/App/Transfer/Plugin/column/year_from_filename.pm
@@ -1,6 +1,6 @@
 package App::Transfer::Plugin::column::year_from_filename;
 
-# ABSTRACT: Transfer plugin: value for field from file name (XXXXyymm)
+# ABSTRACT: Transfer plugin: value for field from file name (TMyymmzz)
 
 use 5.010001;
 use Moose;
@@ -10,11 +10,29 @@ with 'MooX::Log::Any';
 
 sub year_from_filename {
     my ( $self, $p ) = @_;
-    my ( $logstr, $text, $len ) = @$p{qw(logstr value length)};
+    my ( $logstr, $text, $len, $pat ) = @$p{qw(logstr value length pattern)};
+    say "month_from_filename: $text";
     return unless $text;
-    if ( $text =~ m/\w{4}\d{4}/ ) {
-        my $value = substr( $text, 4, 2 );
-        return "20" . $value;
+    die "The 'month_from_filename plugin' requires a pattern attribute" unless $pat;
+    my ($i, $l) = $self->params_from_pattern($pat);
+    say "$i, $l";
+    my $value = '20' . substr( $text, $i, $l );
+    say "return '$value'";
+    return $value;
+}
+
+# pattern = TM[dd]MMDD
+# -> p1 = 2 |
+# -> p2 = 5 | => (2, 2)
+sub params_from_pattern {
+    my ( $self, $pat ) = @_;
+    say "pat = $pat";
+    my $p1 = index $pat, '[';
+    say "p1 = $p1";
+    if ( $p1 > 0 ) {
+        my $p2 = index $pat, ']';
+        say "p2 = $p2";
+        return ( $p1, $p2 - 1 - $p1 );
     }
     return;
 }
